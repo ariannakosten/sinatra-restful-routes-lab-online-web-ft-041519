bade138e75d449d16c5c0d4f02e684b876610b7f
Arianna Kosten <arianna.kosten@gmail.com>
2019-06-25 23:58:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   
   get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
-    erb :
+    erb :edit
   end
  
   patch '/recipes/:id' do 
