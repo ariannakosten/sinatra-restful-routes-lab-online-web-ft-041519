7f142d328243f11f141ca702c1f28d658cc2892e
Arianna Kosten <arianna.kosten@gmail.com>
2019-06-25 23:58:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   
   get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
-    erb :edit
+    erb :
   end
  
   patch '/recipes/:id' do 
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
     redirect to "/recipes/#{@recipe.id}"
   end
   
-  delete '/recipes/:id' do 
+  delete '/recipes/:id/delete' do 
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
     redirect to '/recipes'
